e0f7d146836fa175c87a4e9f3dc4f56ad21a6141
Baktiar Miah <baktiar.m@gmail.com>
2019-08-12 09:02:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |n, num| 
+    p "#{num+1} #{n}"
+  end 
+end
+  
+
+def summon_captain_planet(elements)
+  elements.collect{|n| n.capitalize+"!"}
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.find_all {|n| n.length > 4 }
 end
 
 def find_the_cheese# code an argument here
